fa4bae965c0a0886eb71fc7c0a6fe57c59058ee9
CrownClown00 <rossca.thompson@gmail.com>
2016-06-19 21:35:53 -0400

    bugfixes

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ $(document).ready(function(){
 $("#searchButton").click(function(){
      var   searchTerm=$("#searchField").val();
     //alert(searchTerm);
-     var url ="https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchTerm+"&format=json"
+     var url ="https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchTerm+"&format=json&callback?"
      var callback =function(response){
-         var searchData=response[0][butterflies];
+         //var searchData=response[0][butterflies];
          alert(searchData);
      };
-$.getJSON(url,callback);
+//$.getJSON(url,callback);
+$.ajax( {
+    url,
+    type:"Get",
+    dataType: 'json',
+    success: function(data,status,jqXHR){
+        console.log(data);
+    }
+} );
+
+
 })
 
 
